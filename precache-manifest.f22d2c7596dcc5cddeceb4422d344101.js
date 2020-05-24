1b7368915f8e35a13bf65691d0b50f3434aafd1d
techbayindia <techbayindia@gmail.com>
2020-05-24 18:40:45 +0530

    Updates

---
+++ precache-manifest.f22d2c7596dcc5cddeceb4422d344101.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "81c06995950eaddc3c6e5a51b8c72cf3",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "63d5cf7dd49767ead54f",
+    "url": "/portfolio/static/css/main.cbf71703.chunk.css"
+  },
+  {
+    "revision": "3ebae08122a5f92598f1",
+    "url": "/portfolio/static/js/2.0d1de12b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portfolio/static/js/2.0d1de12b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "63d5cf7dd49767ead54f",
+    "url": "/portfolio/static/js/main.df141a27.chunk.js"
+  },
+  {
+    "revision": "86d362331c7a0edbf17c",
+    "url": "/portfolio/static/js/runtime-main.8d493650.js"
+  }
+]);
