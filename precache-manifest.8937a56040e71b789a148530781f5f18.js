c9aa53befca4dd2f3a8186524cde6b410c3cf7c2
kothawleprem@gmail.com <kothawleprem@gmail.com>
2020-07-31 19:23:06 +0530

    Updates

---
+++ precache-manifest.8937a56040e71b789a148530781f5f18.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f65960d7834427690f897c88e951bb83",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "c54c22696c8cb39832da",
+    "url": "/portfolio/static/css/main.cbf71703.chunk.css"
+  },
+  {
+    "revision": "5b6354854aa418bd22a1",
+    "url": "/portfolio/static/js/2.b7345c27.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portfolio/static/js/2.b7345c27.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c54c22696c8cb39832da",
+    "url": "/portfolio/static/js/main.d8c030d6.chunk.js"
+  },
+  {
+    "revision": "88fbd3d397ae3690d5f2",
+    "url": "/portfolio/static/js/runtime-main.fb1b3dac.js"
+  }
+]);
